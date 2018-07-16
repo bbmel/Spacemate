63f1d2021f9fdbb113a479d8b9883882c8439b1b
Mel <mxg1213@miami.edu>
2018-07-16 12:22:57 -0700

    init commit

---
+++ Spacemate/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Spacemate
+//
+//  Created by Melanie Gravier on 7/16/18.
+//  Copyright © 2018 Mel. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
