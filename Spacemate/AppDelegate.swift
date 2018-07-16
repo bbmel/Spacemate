150ef3c9535ffaaa81b3dea78d304e82559bf607
Mel <mxg1213@miami.edu>
2018-07-16 13:45:14 -0700

    v1.0 of l/r swipe works

--- Spacemate/AppDelegate.swift
+++ Spacemate/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let config = ParseClientConfiguration { (theConfig) in
+            theConfig.applicationId = "spacematemakeschoolhack"
+            theConfig.server = "http://spacemate-server.herokuapp.com/parse"
+            theConfig.clientKey = "notsureifthisisgoingtoworkbutwecantry"
+            
+        }
+        
+        Parse.initialize(with: config)
+        
+        
         return true
     }
 
--- Spacemate/ViewController.swift
+++ Spacemate/ViewController.swift
@@ -10,10 +10,30 @@ import UIKit
 import Parse
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var swipeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let gesture = UIPanGestureRecognizer(target: self, action: #selector(wasDragged(gestureRecognizer:)))
+        swipeLabel.addGestureRecognizer(gesture)
+    }
+    
+    @objc func wasDragged(gestureRecognizer: UIPanGestureRecognizer) {
+        let labelPoint = gestureRecognizer.translation(in: view)
+        swipeLabel.center = CGPoint(x: view.bounds.width / 2 + labelPoint.x, y: view.bounds.height / 2 + labelPoint.y)
+        
+        if gestureRecognizer.state == .ended {
+            if swipeLabel.center.x < (view.bounds.width / 2 - 100) {
+                print("U ugly")
+            }
+            if swipeLabel.center.x > (view.bounds.width / 2 + 100) {
+                print("U look good")
+            }
+            
+            swipeLabel.center = CGPoint(x: view.bounds.width / 2, y: view.bounds.height / 2)
+        }
     }
 
     override func didReceiveMemoryWarning() {
