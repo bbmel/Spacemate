168235bdca7733eabdb1b1f7e02708796f559032
Mel <mxg1213@miami.edu>
2018-07-16 14:09:07 -0700

    swiping action works

--- Spacemate/ViewController.swift
+++ Spacemate/ViewController.swift
@@ -24,6 +24,14 @@ class ViewController: UIViewController {
         let labelPoint = gestureRecognizer.translation(in: view)
         swipeLabel.center = CGPoint(x: view.bounds.width / 2 + labelPoint.x, y: view.bounds.height / 2 + labelPoint.y)
         
+        let xFromCenter = view.bounds.width / 2 - swipeLabel.center.x
+        
+        var rotation = CGAffineTransform(rotationAngle: xFromCenter / 200)
+        let scale = min(100 / abs(xFromCenter), 1)
+        var scaledAndRotated = rotation.scaledBy(x: scale, y: scale)
+        swipeLabel.transform = scaledAndRotated
+        
+        
         if gestureRecognizer.state == .ended {
             if swipeLabel.center.x < (view.bounds.width / 2 - 100) {
                 print("U ugly")
@@ -31,6 +39,11 @@ class ViewController: UIViewController {
             if swipeLabel.center.x > (view.bounds.width / 2 + 100) {
                 print("U look good")
             }
+            
+            // go back to the original position if user has stopped dragging
+            rotation = CGAffineTransform(rotationAngle: 0)
+            scaledAndRotated = rotation.scaledBy(x: 1, y: 1)
+            swipeLabel.transform = scaledAndRotated
             
             swipeLabel.center = CGPoint(x: view.bounds.width / 2, y: view.bounds.height / 2)
         }
