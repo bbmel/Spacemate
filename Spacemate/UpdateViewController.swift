54b9050060902811701fe7da61f467b2baadb21f
Mel <mxg1213@miami.edu>
2018-07-17 14:53:02 -0700

    can upload profile photo

--- Spacemate/UpdateViewController.swift
+++ Spacemate/UpdateViewController.swift
@@ -8,28 +8,39 @@
 
 import UIKit
 
-class UpdateViewController: UIViewController {
-
+class UpdateViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userGenderSwitch: UISwitch!
+    @IBOutlet weak var interestedGenderSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func updateImageTapped(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            profileImageView.image = image
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func updateTapped(_ sender: Any) {
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
